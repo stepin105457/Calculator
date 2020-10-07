49d58bb8827a9ffd71f57853210cf5074af3d5d5
99002774 <72468886+99002774@users.noreply.github.com>
2020-10-07 16:12:11 +0530

    Update header.h

--- 04_Implementation/inc/header.h
+++ 04_Implementation/inc/header.h
@@ -1,2 +1,11 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+void display(float n1, float n2, char ch, float result);
+void add(float n1, float n2);
+void subtract(float n1, float n2);
+void multiply(float n1, float n2);
+void divide(float n1, float n2);
+void rem(float n1, float n2);
+void power(float n1, float n2);
+void area(float n1, float n2);
